d510678da1eed5f0546dfad6e29cd689f22b3d33
Aman Singh Solanki <amans330@gmail.com>
2020-11-10 02:01:43 -0800

    implemented pkce, axios post issue resolved

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,14 +1,18 @@
-
 const express = require('express');
 const router = express.Router();
+const pkce = require('../helpers/pkce');
 
 router.get('/', (req, res) => {
+  // Generate and store the PKCE verifier
+  req.session.verifier = pkce.generateVerifier();
+  // Generate the PKCE challenge
+  const challenge = pkce.generateChallenge(req.session.verifier);
 
   const stateValue = Math.random().toString(36).substring(2,15) + Math.random().toString(36).substring(2,15) + Math.random().toString(36).substring(2,15) + Math.random().toString(36).substring(2,15) + Math.random().toString(36).substring(2,15) + Math.random().toString(36).substring(2,15);
 
   req.session.stateValue = stateValue
 
-  res.redirect(`http://localhost:${process.env.FUSIONAUTH_PORT}/oauth2/authorize?client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}&response_type=code&state=${stateValue}`);
+  res.redirect(`http://localhost:${process.env.FUSIONAUTH_PORT}/oauth2/authorize?client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}&response_type=code&state=${stateValue}&code_challenge=${challenge}&code_challenge_method=S256`);
 });
 
 module.exports = router;
--- server/routes/oauth-callback.js
+++ server/routes/oauth-callback.js
@@ -1,8 +1,45 @@
-
 const express = require("express");
 const router = express.Router();
+const request = require('request');
 const axios = require("axios").default;
 const qs = require("query-string");
+
+// router.get('/', (req, res) => {
+//     const url = `http://localhost:${process.env.FUSIONAUTH_PORT}/oauth2/token`;
+//     const stateFromServer = req.query.state;
+//     console.log("sunny:", url, qs.stringify({
+//         client_id: process.env.CLIENT_ID,
+//         client_secret: process.env.CLIENT_SECRET,
+//         code: req.query.code,
+//         grant_type: "authorization_code",
+//         redirect_uri: process.env.REDIRECT_URI,
+//         state:stateFromServer
+//     }),)
+//     request(
+//         // POST request to /token endpoint
+//         {
+//             method: 'POST',
+//             uri: `http://localhost:${process.env.FUSIONAUTH_PORT}/oauth2/token`,
+//             form: {
+//                 'client_id': process.env.CLIENT_ID,
+//                 'client_secret': process.env.CLIENT_SECRET,
+//                 'code': req.query.code,
+//                 'code_verifier': req.session.verifier,
+//                 'grant_type': 'authorization_code',
+//                 'redirect_uri': process.env.REDIRECT_URI
+//             }
+//         },
+//
+//         // callback
+//         (error, response, body) => {
+//             // save token to session
+//             req.session.token = JSON.parse(body).access_token;
+//
+//             // redirect to Vue app
+//             res.redirect(`http://localhost:8081`);
+//         }
+//     );
+// });
 
 const config = {
   headers: {
@@ -12,39 +49,36 @@ const config = {
 const url = `http://localhost:${process.env.FUSIONAUTH_PORT}/oauth2/token`;
 
 router.get("/", (req, res) => {
-// State from Server
-const stateFromServer = req.query.state;
+    // State from Server
+    const stateFromServer = req.query.state;
 
-if (stateFromServer !== req.session.stateValue) {
-  console.log("State doesn't match. uh-oh.");
-  console.log(`Saw: ${stateFromServer}, but expected: &{req.session.stateValue}`);
-  res.redirect(302, '/');
-  return;
-}
-
-  //post request to /token endpoint
-  axios
-    .post(
+    if (stateFromServer !== req.session.stateValue) {
+      console.log("State doesn't match. uh-oh.");
+      console.log(`Saw: ${stateFromServer}, but expected: &{req.session.stateValue}`);
+      res.redirect(302, '/');
+      return;
+    }
+    //post request to token endpoint
+    axios.post(
       url,
       qs.stringify({
-        client_id: process.env.CLIENT_ID,
-        client_secret: process.env.CLIENT_SECRET,
-        code: req.query.code,
-        grant_type: "authorization_code",
-        redirect_uri: process.env.REDIRECT_URI,
-        state:stateFromServer
+        'client_id': process.env.CLIENT_ID,
+        'client_secret': process.env.CLIENT_SECRET,
+        'code': req.query.code,
+        'code_verifier': req.session.verifier,
+        'grant_type': "authorization_code",
+        'redirect_uri': process.env.REDIRECT_URI
       }),
       config
     )
     .then((result) => {
-
       // save token to session
       req.session.token = result.data.access_token;
-
       //redirect to Vue app
      res.redirect(`http://localhost:8081`);
     })
     .catch((err) => {
+        // console.log("aman")
       console.error(err);
     });
 });
