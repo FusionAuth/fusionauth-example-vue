db6bc2315a7d81e03438e04a1c9d934302852a4f
Joshua O'Bannon <joshua@fusionauth.io>
2021-03-16 16:21:51 -0600

    Update the Server code

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,51 +4,63 @@ const axios = require("axios");
 const qs = require("querystring");
   
 router.get("/", (req, res) => {
-  // token in session -> get user data and send it back to the vue app
-  if (req.session.token) {
-    axios
-      .post(
-        `http://localhost:${process.env.FUSIONAUTH_PORT}/oauth2/introspect`,
-        qs.stringify({
-          client_id: process.env.CLIENT_ID,
-          token: req.session.token,
-        })
-      )
-      .then((result) => {
-        let introspectResponse = result.data;
-        // valid token -> get more user data and send it back to the Vue app
-        if (introspectResponse) {
-          
-          // GET request to /registration endpoint
-          axios
-            .get(
-              `http://localhost:${process.env.FUSIONAUTH_PORT}/api/user/registration/${introspectResponse.sub}/${process.env.APPLICATION_ID}`,
-              {
-                headers: {
-                  Authorization: process.env.API_KEY,
-                },
-              }
+    // token in session -> get user data and send it back to the vue app
+    if (req.session.token) {
+        axios
+            .post(
+                `http://localhost:${process.env.FUSIONAUTH_PORT}/oauth2/introspect`,
+                qs.stringify({
+                    client_id: process.env.CLIENT_ID,
+                    token: req.session.token,
+                })
             )
-            .then((response) => {
-              res.send({
-                introspectResponse: introspectResponse,
-                body: response.data.registration,
-              });
+            .then((result) => {
+                let introspectResponse = result.data;
+                // valid token -> get more user data and send it back to the Vue app
+                if (introspectResponse) {
+
+                    // GET request to /registration endpoint
+                    axios
+                        .get(
+                            `http://localhost:${process.env.FUSIONAUTH_PORT}/api/user/registration/${introspectResponse.sub}/${process.env.APPLICATION_ID}`,
+                            {
+                                headers: {
+                                    Authorization: process.env.API_KEY,
+                                },
+                            }
+                        )
+                        .then((response) => {
+                            res.send({
+                                authState: "Authorized",
+                                introspectResponse: introspectResponse,
+                                body: response.data.registration,
+                            });
+                        })
+                        .catch((err) => {
+                            res.send({
+                                authState: "notAuthorized"
+                            });
+                            console.log(err)
+                            return
+                        })
+                }
+                // expired token -> send nothing
+                else {
+                    req.session.destroy();
+                    res.send({
+                        authState: "notAuthenticated"
+                    });
+                }
             })
-        }
-      // expired token -> send nothing 
-        else {
-          req.session.destroy();
-          res.send({});
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-  // no token -> send nothing
-  else {
-    res.send({});
-  }
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+    // no token -> send nothing
+    else {
+        res.send({
+            authState: "notAuthenticated"
+        });
+    }
 });
 module.exports = router;
