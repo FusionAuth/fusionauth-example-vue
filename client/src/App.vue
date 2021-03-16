d597e4cf23b9052c4c8417364cefd3fb305a297f
Joshua O'Bannon <joshua@fusionauth.io>
2021-03-16 14:29:45 -0600

    Creating sign in for our Vue app

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,39 @@
 <template>
-  <div id="app">
+  <div id='app'>
+    <header>
+      <h1>FusionAuth Example Vue</h1>
+    </header>
+    <div id = 'container'>
+      <Greet v-bind:email="email" />
+    </div>
   </div>
 </template>
-
 <script>
-
+import Greet from './Greeting';
 export default {
-  name: 'App',
+  name: 'app',
   components: {
+      Greet,
+  },
+  data(){
+    return {
+      // email : 'dinesh@fusionauth.io'
+    }
   }
-}
+};
 </script>
-
 <style>
+h1 {
+  text-align: center;
+  font-size: 40px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+#container{
+  box-sizing: border-box;
+  border: 5px solid gray ;
+  border-radius: 15%;
+  width: 400px;
+  height: 400px;
+  margin: auto;
+}
 </style>
